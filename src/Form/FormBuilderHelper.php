48f29edf92c3e06498670ef9826e08177290777a
George van Engers <vanengers@gmail.com>
2025-04-11 17:00:53 +0200

    Release 1.0.3 - Remove from form with parent

--- src/Form/FormBuilderHelper.php
+++ src/Form/FormBuilderHelper.php
@@ -9,11 +9,15 @@ class FormBuilderHelper
     /**
      * @param FormBuilderInterface $form
      * @param string $name
+     * @param string|null $parentName
      * @return bool
      * @author George van Engers <george@dewebsmid.nl>
      * @since 11-04-2025
      */
-    public static function removeForm(FormBuilderInterface $form, string $name): bool
+    public static function removeForm(
+        FormBuilderInterface $form,
+        string $name
+    ): bool
     {
         if ($form->has($name)) {
             $form->remove($name);
@@ -22,6 +26,34 @@ class FormBuilderHelper
         else {
             foreach($form->all() as $child) {
                 if (self::removeForm($child, $name)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param FormBuilderInterface $form
+     * @param string $name
+     * @param string|null $parentName
+     * @return bool
+     * @author George van Engers <george@dewebsmid.nl>
+     * @since 11-04-2025
+     */
+    public static function removeFormWithParent(
+        FormBuilderInterface $form,
+        string $name,
+        ?string $parentName = null
+    ): bool
+    {
+        if ($form->has($parentName)) {
+            return self::removeForm($form->get($parentName), $name);
+        }
+        else {
+            foreach($form->all() as $child) {
+                if (self::removeFormWithParent($child, $name)) {
                     return true;
                 }
             }
